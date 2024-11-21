40d506c3ddfa790fd00dc4c7a5813402c3248432
chilmi89 <achmadchilmi87@gmail.com>
2024-11-21 09:59:11 +0700

    menyambungakan controller ke route dengan fungsi index

--- app/Http/Controllers/Homecontroller.php
+++ app/Http/Controllers/Homecontroller.php
@@ -11,7 +11,7 @@ class Homecontroller extends Controller
      */
     public function index()
     {
-        //
+        return view('welcome');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\Homecontroller;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [Homecontroller::class, 'index']);
